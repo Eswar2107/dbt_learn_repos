da998409fd5ccdc274e703d2884de9301cd04e69
Eswar2107 <eshukarthikeyan2107@gmail.com>
2026-02-16 16:31:32 +0100

    added 2 models and one macro

---
+++ models/staging/dq_null_check_expense_claims.sql
@@ -0,0 +1,19 @@
+-- 2nd model (dq_null_check_expense_claims.sql)
+
+{{ config(
+    materialized='table',
+    database='RAW',
+    alias='DQ_NULL_CHECK_EXPENSE_CLAIMS'
+) }}
+
+WITH dq_check AS (
+    {{ check_nulls(
+        ref('stg_expense_claims'),
+        ['claim_id', 'employee_id', 'claimed_amount']
+    ) }}
+)
+
+SELECT *
+FROM dq_check
+WHERE null_check_status = 'FAIL'
+
